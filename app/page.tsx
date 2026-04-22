8e68a97a74d18e69a11e5266e910b809d9df89ac
Natalino Gomes <intgomes657@gmail.com>
2026-04-22 22:00:33 +0100

    updt

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl text-amber-50 pb-2 pt-2">Hello world </h1>
-      <Link href="/users" className="text-xl text-amber-50 pb-3 pt-2">
+      <Link
+        href="/users"
+        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+      >
         Users
       </Link>
       <ProductCard />
